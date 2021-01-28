3ff96a45e4049b8bcad4e8dad392c209de61a0da
sriram330 <srirams3762@gmail.com>
2021-01-28 10:10:56 +0530

    Day 2 of 100 [Constructors]

---
+++ javaSessions1/src/Constructors/Emp_Details.java
@@ -0,0 +1,47 @@
+/* A constructor initializes an object when it is created.
+
+ Constructors are used to give initial values to the instance variables defined by the class
+
+ Java automatically provides a default constructor that initializes all member variables to zero.
+ However, once you define your own constructor, the default constructor is no longer used.
+
+Types:
+1. No Arguement Constructors:
+Using these constructors the instance variables of a method will be initialized with fixed values for all objects.
+
+2. Paramterized Constructors
+Accepts a specific number of parameters. To initialize data members of a class with distinct values.
+ */
+
+package Constructors;
+public class Emp_Details {
+
+    String name;
+    String Designation;
+    int age;
+
+    //No arguement Constructor
+    public Emp_Details(){
+        String name = "Khan";
+        System.out.println("Original Name is " +name);
+    }
+
+//Parameterized Constructor
+public Emp_Details(String name,int age, String Designation){
+
+    //this keyword is used to refer to the instance variable of a class.
+
+    this.name = name;
+    this.age=age;
+    this.Designation=Designation;
+}
+
+public static void main(String[] args){
+        Emp_Details obj=new Emp_Details();
+
+    Emp_Details obj1= new Emp_Details("Arun",29,"Employee");
+
+    System.out.println(obj1.name +" "+obj1.age +" "+obj1.Designation+" ");
+
+}
+}
---
+++ javaSessions1/src/javaSessions/Interfaces.java
@@ -0,0 +1,49 @@
+package javaSessions;
+
+//Data abstraction is the process of hiding certain details and showing only essential information to the user.
+//Abstraction can be achieved with either abstract classes or interfaces
+
+
+
+
+//An interface is a completely "abstract class" that is used to group related methods with empty bodies
+//Interface methods do not have a body - the body is provided by the "implement" class
+//Interface methods are by default abstract and public
+
+//declare an interface
+interface Animal {
+    public void eat();
+    public void travel();
+    public int noOfLegs();
+}
+
+class MammalInt implements Animal {
+
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    public void travel() {
+        System.out.println("Mammal travels");
+    }
+
+    public int noOfLegs() {
+        return 4;
+    }
+
+    public static void main(String args[]) {
+        MammalInt m = new MammalInt();
+        m.eat();
+        m.travel();
+        System.out.println(m.noOfLegs());
+    }
+}
+
+
+
+
+
+
+
+
+
